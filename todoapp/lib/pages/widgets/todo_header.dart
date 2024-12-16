684b6b7da1338fa9420fbef0c8c82579a11c4ead
southglory <devramyun@gmail.com>
2024-12-16 13:43:30 +0900

    119. themeProvider

--- todoapp/lib/pages/widgets/todo_header.dart
+++ todoapp/lib/pages/widgets/todo_header.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../providers/active_todo_count/active_todo_count_provider.dart';
+import '../providers/theme/theme_provider.dart';
 import '../providers/todo_list/todo_list_provider.dart';
 
 class TodoHeader extends ConsumerWidget {
@@ -13,19 +14,30 @@ class TodoHeader extends ConsumerWidget {
     final todos = ref.watch(todoListProvider);
 
     return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        const Text(
-          'TODO',
-          style: TextStyle(fontSize: 36.0),
+        Row(
+          children: [
+            const Text(
+              'TODO',
+              style: TextStyle(fontSize: 36.0),
+            ),
+            const SizedBox(width: 10),
+            Text(
+              '($activeTodoCount/${todos.length} item${activeTodoCount != 1 ? "s" : ""} left)',
+              style: TextStyle(
+                fontSize: 18.0,
+                color: Colors.blue[900],
+              ),
+            ),
+          ],
         ),
-        const SizedBox(width: 10),
-        Text(
-          '($activeTodoCount/${todos.length} item${activeTodoCount != 1 ? "s" : ""} left)',
-          style: TextStyle(
-            fontSize: 18.0,
-            color: Colors.blue[900],
-          ),
-        ),
+        IconButton(
+          onPressed: () {
+            ref.read(themeProvider.notifier).toggleTheme();
+          },
+          icon: const Icon(Icons.light_mode),
+        )
       ],
     );
   }
