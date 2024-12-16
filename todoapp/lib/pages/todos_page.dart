bf00d3edb367b8ed55940c1d234b9c40540e0c37
southglory <devramyun@gmail.com>
2024-12-16 13:25:03 +0900

    113. FilterTodo widget

--- todoapp/lib/pages/todos_page.dart
+++ todoapp/lib/pages/todos_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'widgets/filter_todo.dart';
 import 'widgets/new_todo.dart';
 
 import 'widgets/search_todo.dart';
@@ -20,6 +21,8 @@ class TodosPage extends StatelessWidget {
               NewTodo(),
               SizedBox(height: 20),
               SearchTodo(),
+              SizedBox(height: 10),
+              FilterTodo(),
             ],
           ),
         ),
